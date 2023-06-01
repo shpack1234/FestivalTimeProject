71d39b488cb852a87de8b5e56efcdf802e90fbfd
shpack1234 <46618113+shpack1234@users.noreply.github.com>
2023-06-02 04:02:15 +0900

    Last Commit Test

--- app/src/main/java/com/festivaltime/festivaltimeproject/MainActivity.java
+++ app/src/main/java/com/festivaltime/festivaltimeproject/MainActivity.java
@@ -10,6 +10,13 @@ class Hello {
         System.out.println("Hello World");
     }
 }
+
+class HelloTwice extends Hello {
+    HelloTwice() {
+        super();
+        System.out.println("Hello World");
+    }
+}
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
