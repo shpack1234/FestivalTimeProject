9755335213fa74f74d2e15426684432a46795ab6
shpack1234 <46618113+shpack1234@users.noreply.github.com>
2023-06-04 23:31:25 +0900

    2023.06.04
    SearchView 만드는중

--- app/src/main/java/com/festivaltime/festivaltimeproject/MapActivity.java
+++ app/src/main/java/com/festivaltime/festivaltimeproject/MapActivity.java
@@ -1,14 +1,14 @@
 package com.festivaltime.festivaltimeproject;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class MapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_map);
     }
 }
---
+++ app/src/main/java/com/festivaltime/festivaltimeproject/SearchActivity.java
@@ -0,0 +1,7 @@
+package com.festivaltime.festivaltimeproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity {
+
+}
