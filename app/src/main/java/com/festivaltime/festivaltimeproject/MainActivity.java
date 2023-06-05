104af518058db5d76d37bc7a91fc52f8f5d12be3
shpack1234 <46618113+shpack1234@users.noreply.github.com>
2023-06-05 13:21:01 +0900

    2023.06.06
    MainActivity 검색 후 SearchActivity 로 query 전달

--- app/src/main/java/com/festivaltime/festivaltimeproject/MainActivity.java
+++ app/src/main/java/com/festivaltime/festivaltimeproject/MainActivity.java
@@ -1,14 +1,43 @@
 package com.festivaltime.festivaltimeproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private SearchView searchView;
+    private String query;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchView = findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                query = s;
+                performSearch(query);
+                navigateToSearchActivity();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+    }
+    private void performSearch(String query) {
+        System.out.println("검색어: " + query);
+    }
+
+    private void navigateToSearchActivity() {
+        Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+        intent.putExtra("query", query);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/festivaltime/festivaltimeproject/SearchActivity.java
+++ app/src/main/java/com/festivaltime/festivaltimeproject/SearchActivity.java
@@ -1,7 +1,21 @@
 package com.festivaltime.festivaltimeproject;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SearchActivity extends AppCompatActivity {
 
+    private TextView queryTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        queryTextView = findViewById(R.id.search_text_View);
+        String query = getIntent().getStringExtra("query");
+        queryTextView.setText(query);
+    }
 }
