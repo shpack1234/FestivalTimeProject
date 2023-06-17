b5a074299e201a3835d5c87c8e04c90534724163
shpack1234 <shpack1234@naver.com>
2023-06-17 19:02:33 +0900

    2023.06.17
    MapActivity 구현 진행중

--- app/src/main/java/com/festivaltime/festivaltimeproject/MapActivity.java
+++ app/src/main/java/com/festivaltime/festivaltimeproject/MapActivity.java
@@ -10,25 +10,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.Signature;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
+import android.util.Pair;
 import android.view.ViewGroup;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MapActivity extends AppCompatActivity implements MapView.CurrentLocationEventListener, MapView.MapViewEventListener {
-    private ViewGroup mapViewContainer;
+public class MapActivity extends AppCompatActivity implements MapView.CurrentLocationEventListener, MapView.MapViewEventListener, MapView.POIItemEventListener {
+    private MapView mapView;
+
+    private List<Pair<Double,Double>> coordinates=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,13 +55,14 @@ public class MapActivity extends AppCompatActivity implements MapView.CurrentLoc
         }
 
 
-
         MapView.setMapTilePersistentCacheEnabled(false);
-        MapView mapView=new MapView(this);
-        mapViewContainer=(ViewGroup)findViewById(R.id.map_view);
+        mapView = new MapView(this);
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
         mapView.setMapViewEventListener(this);
         mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
+        mapView.setPOIItemEventListener(this);
+        onMapViewInitialized(mapView);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.action_map);
@@ -90,7 +94,7 @@ public class MapActivity extends AppCompatActivity implements MapView.CurrentLoc
                     break;
                 }
             }
-            if (check_result == false) {
+            if (!check_result) {
                 finish();
             }
         }
@@ -118,8 +122,15 @@ public class MapActivity extends AppCompatActivity implements MapView.CurrentLoc
 
     @Override
     public void onMapViewInitialized(MapView mapView) {
+        mapView.setZoomLevel(11, true);
+        coordinates.add(new Pair<>(37.5665, 126.9780));
+        coordinates.add(new Pair<>(33.3949, 126.5614));
 
+        MapPOIItem[] marker={new MapPOIItem(), new MapPOIItem()};
+
+        marKingFestivalGroup(marker);
     }
+
 
     @Override
     public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
@@ -128,7 +139,7 @@ public class MapActivity extends AppCompatActivity implements MapView.CurrentLoc
 
     @Override
     public void onMapViewZoomLevelChanged(MapView mapView, int i) {
-
+        Log.d("MapActivity", "Current Zoom Level"+i);
     }
 
     @Override
@@ -158,6 +169,42 @@ public class MapActivity extends AppCompatActivity implements MapView.CurrentLoc
 
     @Override
     public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
+
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem) {
+
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) {
+
+    }
+
+    @Override
+    public void onDraggablePOIItemMoved(MapView mapView, MapPOIItem mapPOIItem, MapPoint mapPoint) {
+
+    }
+
+    private void marKingFestivalGroup(@NonNull MapPOIItem[] marker) {
+        Bitmap originalMarkerBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.map_marking);
+        Bitmap resizedMarkerBitmap = Bitmap.createScaledBitmap(originalMarkerBitmap, 200, 200, false);
+        for(int i=0; i<marker.length; i++) {
+            marker[i].setItemName("마커");
+            marker[i].setTag(i);
+            marker[i].setMapPoint(MapPoint.mapPointWithGeoCoord(coordinates.get(i).first, coordinates.get(i).second));
+            marker[i].setMarkerType(MapPOIItem.MarkerType.CustomImage);
+            marker[i].setCustomImageBitmap(resizedMarkerBitmap);
+            marker[i].setCustomImageAutoscale(false);
+            marker[i].setCustomImageAnchor(0.5f, 1.0f);
+            mapView.addPOIItem(marker[i]);
+        }
 
     }
 }
