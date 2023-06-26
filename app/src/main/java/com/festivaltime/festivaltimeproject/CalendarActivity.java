17ee875af3b24a93ca2f0d951f1deb67a02064f3
shpack1234 <shpack1234@naver.com>
2023-06-26 12:47:14 +0900

    2023.06.26
    CalendarActivity 하단바 추가

--- app/src/main/java/com/festivaltime/festivaltimeproject/CalendarActivity.java
+++ app/src/main/java/com/festivaltime/festivaltimeproject/CalendarActivity.java
@@ -1,6 +1,41 @@
 package com.festivaltime.festivaltimeproject;
 
+import static com.festivaltime.festivaltimeproject.navigateToSomeActivity.navigateToCalendarActivity;
+import static com.festivaltime.festivaltimeproject.navigateToSomeActivity.navigateToFavoriteActivity;
+import static com.festivaltime.festivaltimeproject.navigateToSomeActivity.navigateToMainActivity;
+import static com.festivaltime.festivaltimeproject.navigateToSomeActivity.navigateToMapActivity;
+import static com.festivaltime.festivaltimeproject.navigateToSomeActivity.navigateToMyPageActivity;
+
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class CalendarActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.action_calendar);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.action_home) {
+                navigateToMainActivity(CalendarActivity.this);
+                return true;
+            } else if (item.getItemId() == R.id.action_map) {
+                navigateToMapActivity(CalendarActivity.this);
+                return false;
+            } else if (item.getItemId() == R.id.action_calendar) {
+                return false;
+            } else if (item.getItemId() == R.id.action_favorite) {
+                navigateToFavoriteActivity(CalendarActivity.this);
+                return true;
+            } else {
+                navigateToMyPageActivity(CalendarActivity.this);
+                return true;
+            }
+        });
+    }
 }
