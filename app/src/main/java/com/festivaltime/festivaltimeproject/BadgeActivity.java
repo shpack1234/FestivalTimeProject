7df720828ee8ccd52f710b1e976abaf11d7b887c
seohyun020633 <seohyun020633@gmail.com>
2023-06-29 11:12:00 +0900

    2023.06.29
    mybadge화면 만듬

---
+++ app/src/main/java/com/festivaltime/festivaltimeproject/BadgeActivity.java
@@ -0,0 +1,45 @@
+package com.festivaltime.festivaltimeproject;
+
+import static com.festivaltime.festivaltimeproject.navigateToSomeActivity.navigateToCalendarActivity;
+import static com.festivaltime.festivaltimeproject.navigateToSomeActivity.navigateToMainActivity;
+import static com.festivaltime.festivaltimeproject.navigateToSomeActivity.navigateToMapActivity;
+import static com.festivaltime.festivaltimeproject.navigateToSomeActivity.navigateToMyPageActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class BadgeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_badge);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);//하단 바 navigate 처리
+        bottomNavigationView.setSelectedItemId(R.id.action_profile);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.action_home:
+                    navigateToMainActivity(BadgeActivity.this);
+                    return true;
+                case R.id.action_map:
+                    navigateToMapActivity(BadgeActivity.this);
+                    return true;
+                case R.id.action_calendar:
+                    navigateToCalendarActivity(BadgeActivity.this);
+                    return true;
+                case R.id.action_favorite:
+                    return true;
+                case R.id.action_profile:
+                    navigateToMyPageActivity(BadgeActivity.this);
+                    return true;
+            }
+            return false;
+        });
+
+    }
+}
+
