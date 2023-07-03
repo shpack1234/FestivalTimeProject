ae2a0544c67f35538ff2e27cb455805898710c29
yj2da <oyjcat@naver.com>
2023-07-03 21:05:48 +0900

    2023.07.03
    1. 축제 캘린더 다시 건축
    2. 개인 캘린더-축제 캘린더 연결
    
    (수정 예정)
    1. 날짜 선택시 색상 유지되는 현상
    2. 날짜 선택시 일정 정보 숨김처리 미해제 현상(커스텀 캘린더 적용해서 그럼)
    3. 팝업-카테고리 커스텀 캘린더에 미적용 (기존 캘린더 정보받아왔었음)
    
    (추가예정)
    1. 캘린더 설정 팝업(이전달표시, 다음달도 표시추가 예정)
    2. 기타 데이터 주고받기(카테고리, 날짜별 일정등등)
    
    (내일 예정)
    1. 캘린더 설정 팝업
    2. (추가적으로 보고 결정)
    
    추가남길말: 오늘은 여기까지만 할게요. 더 많이하면 멘탈 나가서 못함. 그리고 프론트 먼저하고 백엔드쪽 할게요. 에휴.에휴.에휴.에휴.에휴. 주석 내일 상세히 남길게요. 추가로 내일 일정있어서 오후 10시는 넘어서 올리겠습니다. 모레에 많이 할게요.
    #캘린더_1인개발_해명해 #혹사 #노조 #시위

--- app/src/main/java/com/festivaltime/festivaltimeproject/FestivalCalendarAdapter.java
+++ app/src/main/java/com/festivaltime/festivaltimeproject/FestivalCalendarAdapter.java
@@ -15,55 +15,49 @@ import java.util.Date;
 public class FestivalCalendarAdapter extends RecyclerView.Adapter<FestivalCalendarAdapter.FestivalCalendarViewHolder> {
     ArrayList<Date> dayList;
 
-    public FestivalCalendarAdapter(ArrayList<Date> dayList) {
+    public FestivalCalendarAdapter(ArrayList<Date> dayList){
         this.dayList = dayList;
     }
 
     @NonNull
     @Override
-    public FestivalCalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public FestivalCalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.festivalcalendar_cell, parent, false);
         return new FestivalCalendarViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull FestivalCalendarViewHolder holder, int position) {
-        Date monthDate = dayList.get(position); //날짜 변수 적용
+    public void onBindViewHolder(@NonNull FestivalCalendarViewHolder holder, int position){
+        Date monthDate = dayList.get(position);
         Calendar dateCalendar = Calendar.getInstance();
         dateCalendar.setTime(monthDate);
 
-        int currentDay = CalendarUtil.selectedDate.get(Calendar.DAY_OF_MONTH);
-        int currentMonth = CalendarUtil.selectedDate.get(Calendar.MONTH) + 1;
+        int currentMonth = CalendarUtil.selectedDate.get(Calendar.MONTH)+1;
         int currentYear = CalendarUtil.selectedDate.get(Calendar.YEAR);
 
-        int displayDay = dateCalendar.get(Calendar.DAY_OF_MONTH);
-        int displayMonth = dateCalendar.get(Calendar.MONTH) + 1;
+        int displayMonth = dateCalendar.get(Calendar.MONTH)+1;
         int displayYear = dateCalendar.get(Calendar.YEAR);
 
-        if (displayMonth == currentMonth && displayYear == currentYear) {
-        } else {
-            holder.parentView.setVisibility(View.INVISIBLE);
+        if(displayMonth==currentMonth && displayYear==currentYear){
         }
+        else{holder.parentView.setVisibility(View.INVISIBLE);}
 
         int dayNo = dateCalendar.get(Calendar.DAY_OF_MONTH);
         holder.dayText.setText(String.valueOf(dayNo));
 
-        //날짜 클릭 이벤트
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //클릭 기능 구현
             }
         });
     }
 
     @Override
-    public int getItemCount() {
-        return dayList.size();
-    }
+    public int getItemCount(){return dayList.size();}
 
     class FestivalCalendarViewHolder extends RecyclerView.ViewHolder {
-        //초기화
         TextView dayText;
         View parentView;
 
@@ -73,5 +67,7 @@ public class FestivalCalendarAdapter extends RecyclerView.Adapter<FestivalCalend
             parentView = itemView.findViewById(R.id.parentView);
         }
     }
+
+
 
 }
