1c8e65484880b3b2559a7c60033127aefe383e58
mnny25 <nayoung4455@naver.com>
2023-07-10 16:40:07 +0900

    2023.07.10
    앱 설정 액티비티 만듦
    
    * 처음에 코틀린으로 파일 잘못 만들어서 코틀린 관련한게 gradle에 떠서 다 지웠어요 ㅠㅠ,,, 죄삼다

---
+++ app/src/main/java/com/festivaltime/festivaltimeproject/AppSetting.java
@@ -0,0 +1,14 @@
+package com.festivaltime.festivaltimeproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AppSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_setting);
+    }
+}
