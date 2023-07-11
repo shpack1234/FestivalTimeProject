2497e03b205440159580c5c91b6454bc3cf8b4ce
seohyun020633 <seohyun020633@gmail.com>
2023-07-11 11:10:26 +0900

    2023.07.11
    더보기 버튼 추가했는데 마음에 안듬...

--- app/src/main/java/com/festivaltime/festivaltimeproject/InquiryActivity.java
+++ app/src/main/java/com/festivaltime/festivaltimeproject/InquiryActivity.java
@@ -12,31 +12,61 @@ import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class InquiryActivity extends AppCompatActivity {
+
+    private TextView inquiryText;
+    private TextView moreButton;
+    private LinearLayout inquiryLayout;
+    private boolean isExpanded = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inquiry);
 
-        TextView textView = findViewById(R.id.user_inquiry1);
-        textView.setOnClickListener(new View.OnClickListener() {
+        inquiryText = findViewById(R.id.inquiry1_text);
+        moreButton = findViewById(R.id.moreButton);
+        inquiryLayout = findViewById(R.id.inquiry1_layout);
+
+        // 초기에는 접혀진 상태로 시작
+        collapseLayout();
+
+        // Layout 클릭 시 확장/축소 동작
+        inquiryLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView textView = (TextView) v;
-                if (textView.getMaxLines() >= 2) {
-                    textView.setMaxLines(Integer.MAX_VALUE);
-                    textView.setEllipsize(null);
+                if (isExpanded) {
+                    collapseLayout();
                 } else {
-                    textView.setMaxLines(2);
-                    textView.setEllipsize(TextUtils.TruncateAt.END);
+                    expandLayout();
                 }
+            }
+        });
+
+        // ...더보기 버튼 클릭 시 확장 동작
+        moreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                expandLayout();
             }
         });
     }
 
-    
+    private void expandLayout() {
+        inquiryText.setMaxLines(Integer.MAX_VALUE);
+        moreButton.setVisibility(View.GONE);
+        isExpanded = true;
+    }
 
-
+    private void collapseLayout() {
+        inquiryText.setMaxLines(2);
+        moreButton.setVisibility(View.VISIBLE);
+        isExpanded = false;
+    }
 }
+
