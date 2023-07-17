f4aebafbe7e39e1bf4892c6d1dfc64cf46639050
shpack1234 <shpack1234@naver.com>
2023-07-17 15:31:42 +0900

    2023.07.17
    축제 분류 가능한 searchKeyword 메소드 오버로딩

--- app/src/main/java/com/festivaltime/festivaltimeproject/ApiReader.java
+++ app/src/main/java/com/festivaltime/festivaltimeproject/ApiReader.java
@@ -70,6 +70,49 @@ public class ApiReader {
         }
 
     }
+    public void searchKeyword(String serviceKey, String keyword, String cat, final ApiResponseListener listener){
+        try {
+            HttpUrl.Builder urlBuilder = new HttpUrl.Builder() // 수정된 부분
+                    .scheme("https")
+                    .host("apis.data.go.kr")
+                    .addPathSegment("B551011")
+                    .addPathSegment("KorService1")
+                    .addPathSegment("searchKeyword1")
+                    .addQueryParameter("numOfRows", "10")
+                    .addQueryParameter("MobileOS", "AND")
+                    .addQueryParameter("MobileApp", "FestivalTime")
+                    .addQueryParameter("keyword", keyword)
+                    .addQueryParameter("contentTypeId", "15")
+                    .addQueryParameter("serviceKey", serviceKey)
+                    .addQueryParameter("cat3", cat);
+
+            String url = urlBuilder.build().toString();
+            Log.d("url", url);
+            Request request = new Request.Builder().url(url).build();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    e.printStackTrace();
+                    listener.onError("Network Error");
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    if (response.isSuccessful()) {
+                        String responseData = response.body().string();
+                        listener.onSuccess(responseData);
+                    } else {
+                        listener.onError("Server Error: " + response.code());
+                    }
+                }
+            });
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            listener.onError("URL Encoding Error");
+        }
+
+    }
     public void detailCommon(String serviceKey, String contentId, final ApiResponseListener listener) {
         try {
             HttpUrl.Builder urlBuilder = new HttpUrl.Builder() // 수정된 부분
