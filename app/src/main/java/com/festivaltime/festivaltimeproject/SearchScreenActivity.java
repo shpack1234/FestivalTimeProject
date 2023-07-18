8ed299b22fd36516dff33fde2c12113ec0add5a6
mnny25 <nayoung4455@naver.com>
2023-07-19 02:15:22 +0900

    2023.07.19
    뭔가 여러 시도를 햇으나... 다 실패해서 일단 축제 컨테이너 틀만 커밋 ㅠㅠ

--- app/src/main/java/com/festivaltime/festivaltimeproject/SearchScreenActivity.java
+++ app/src/main/java/com/festivaltime/festivaltimeproject/SearchScreenActivity.java
@@ -3,29 +3,44 @@ package com.festivaltime.festivaltimeproject;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class SearchScreenActivity extends AppCompatActivity {
 
     String type;
+
+    private ApiReader apiReader;
+    private List<HashMap<String, String>> festivalList = new ArrayList<>();
+    private Executor executor;
+    private LinearLayout festivalContainer2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_screen);
 
+        executor = Executors.newSingleThreadExecutor();
+
         type="A02080200";
 
         String query = getIntent().getStringExtra("query");
 
-        Button entireButton=findViewById(R.id.detail_search_button);
+        Button entireButton = findViewById(R.id.detail_search_button);
         entireButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 navigateToSomeActivity.navigateToSearchDetailActivity(SearchScreenActivity.this, query, type);
             }
         });
-    }
 
+
+    }
 }
