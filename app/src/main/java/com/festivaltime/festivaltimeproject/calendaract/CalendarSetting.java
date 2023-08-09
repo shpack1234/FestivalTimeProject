474776175d5b9e4b4c4abfb71b01856c7feb12b2
yj2da <oyjcat@naver.com>
2023-08-09 17:03:34 +0900

    2023.08.09
    설정 다이얼로그 카테고리 레이아웃 추가

--- app/src/main/java/com/festivaltime/festivaltimeproject/calendaract/CalendarSetting.java
+++ app/src/main/java/com/festivaltime/festivaltimeproject/calendaract/CalendarSetting.java
@@ -8,19 +8,30 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 import com.festivaltime.festivaltimeproject.R;
+import com.festivaltime.festivaltimeproject.calendarcategorydatabasepackage.CalendarCategoryDao;
+import com.festivaltime.festivaltimeproject.calendarcategorydatabasepackage.CalendarCategoryDataBase;
+import com.festivaltime.festivaltimeproject.calendarcategorydatabasepackage.CalendarCategoryEntity;
+import com.festivaltime.festivaltimeproject.calendarcategorydatabasepackage.FetchCategoryTask;
+import com.festivaltime.festivaltimeproject.calendardatabasepackage.CalendarEntity;
+
+import java.util.List;
 
 //캘린더 설정 dialog class 카테고리 관리, 카테고리별/날짜별 표시 기능
-public class CalendarSetting extends Dialog {
+public class CalendarSetting extends Dialog implements FetchCategoryTask.FetchCategoryTaskListener{
+    private CalendarCategoryDao categoryDao;
+    private LinearLayout categoryContainer;
     final Switch othermonth_switch;
-    final  boolean showOtherMonths;
+    final boolean showOtherMonths;
     protected Context mContext;
     public Button finish_btn, add_category;
-    private CalendarCategory Add_category;
 
     public CalendarSetting(@NonNull Context context, boolean showOtherMonths) {
         super(context);
@@ -33,6 +44,11 @@ public class CalendarSetting extends Dialog {
         finish_btn = findViewById(R.id.add_finish_btn);
         add_category = findViewById(R.id.add_category);
         othermonth_switch = findViewById(R.id.othermonth_switch);
+
+        categoryDao = CalendarCategoryDataBase.getInstance(mContext).categoryDao();
+
+        FetchCategoryTask fetchCategoryTask = new FetchCategoryTask(mContext, categoryDao);
+        fetchCategoryTask.fetchCategories(this);
 
         setCancelable(true);
         setCanceledOnTouchOutside(true);
@@ -87,6 +103,32 @@ public class CalendarSetting extends Dialog {
         }
 
     }
+
+    @Override
+    public void onFetchCompleted(List<CalendarCategoryEntity> categorylist) {
+        updateUI(categorylist); // 매개변수를 전달하여 호출합니다.
+    }
+
+    // 화면을 업데이트하는 메서드
+    private void updateUI(List<CalendarCategoryEntity> categorylist) {
+        LinearLayout categoryContainer = findViewById(R.id.category_container);
+        categoryContainer.removeAllViews();
+
+        for (CalendarCategoryEntity category : categorylist) {
+            View categoryBox = getLayoutInflater().inflate(R.layout.category_box, null);
+            TextView titleTextView = categoryBox.findViewById(R.id.category_box_text);
+            //ImageButton deleteButton = categoryBox.findViewById(R.id.schedule_deleteButton);
+
+            String title = category.name;
+
+            // 일정 데이터를 각 scheduleBox에 담는 작업
+            titleTextView.setText(title);
+
+            // 각 scheduleBox를 scheduleContainer에 추가
+            categoryContainer.addView(categoryBox);
+        }
+    }
+
     public boolean getShowOtherMonths() {
         return othermonth_switch.isChecked();
     }
