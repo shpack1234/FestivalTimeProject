c5e22c96a94c8d88cfef7c2a6f77525e67e8187c
yj2da <oyjcat@naver.com>
2023-08-18 23:23:35 +0900

    2023.08.18
    일정있으면 점표시 하려하는데 왤케 안되는것이며. 학원은 왜자꾸 연장근무를 시켜서 나를 바쁘게 하는 것일까요.

--- app/src/main/java/com/festivaltime/festivaltimeproject/calendardatabasepackage/FetchScheduleTask.java
+++ app/src/main/java/com/festivaltime/festivaltimeproject/calendardatabasepackage/FetchScheduleTask.java
@@ -2,8 +2,14 @@ package com.festivaltime.festivaltimeproject.calendardatabasepackage;
 
 import android.content.Context;
 import android.view.View;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -30,8 +36,37 @@ public class FetchScheduleTask {
         });
     }
 
+    // 해당 날짜에 속하는 일정 데이터를 가져오는 메소드
+    public List<CalendarEntity> fetchSchedulesForDate(Date date) {
+        List<CalendarEntity> allSchedules = calendarDao.getAllCalendarEntity();
+        List<CalendarEntity> matchingSchedules = new ArrayList<>();
+
+        for (CalendarEntity schedule : allSchedules) {
+            Date startDate = convertStringToDate(schedule.startDate);
+            Date endDate = convertStringToDate(schedule.endDate);
+
+            if (startDate != null && endDate != null && date.after(startDate) && date.before(endDate)) {
+                matchingSchedules.add(schedule);
+            }
+        }
+
+        return matchingSchedules;
+    }
+
+    // 문자열 형태의 날짜를 Date 객체로 변환하는 메소드
+    private Date convertStringToDate(String dateString) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd", Locale.getDefault());
+        try {
+            return dateFormat.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     // FetchScheduleTask에서 일정 데이터를 가져온 후, CalendarActivity에 전달하는 리스너 인터페이스
     public interface FetchScheduleTaskListener {
         void onFetchCompleted(List<CalendarEntity> scheduleList);
     }
+
 }
